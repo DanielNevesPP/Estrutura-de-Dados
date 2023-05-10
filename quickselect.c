a036d0cee484a1a670de970e7abb520f4a394e18
DanielNevesPP <85492821+DanielNevesPP@users.noreply.github.com>
2023-05-10 16:44:28 -0300

    Update quickselect.c

--- quickselect.c
+++ quickselect.c
@@ -2,11 +2,8 @@
 #include <stdlib.h>
 
 //funcao que troca o valor de duas variaveis
-void troca(int x,int y){
-    int temp = x;
-    x = y;
-    y = temp;
-}
+#define troca(x, y) {int temp = x; x = y; y = temp;}
+
 
 //faz a particao do vetor
 int particiona(int *vetor, int esq, int dir, int pivo_index){
@@ -15,7 +12,13 @@ int particiona(int *vetor, int esq, int dir, int pivo_index){
     //move o pivo para o final do vetor
     troca(vetor[pivo_index], vetor[dir]);
 
+    //pivo_index recebe o valor de esq
     pivo_index = esq;
+
+    /*cada vez que eh encontrado um elemento menor ou igual ao pivo, pivo_index eh
+    incrementado, e esse elemento sera colocado antes do indice do pivo, assim, todos os 
+    elementos menores que o pivo ficarao a esquerda de pivo_index e os maiores ficarao a 
+    direita*/
     int i;
     for(i = esq; i < dir; i++){
         if(vetor[i] <= pivo){
@@ -23,29 +26,61 @@ int particiona(int *vetor, int esq, int dir, int pivo_index){
             pivo_index++;
         }
     }
+    //move o pivo para sua posicao final
     troca(vetor[pivo_index], vetor[dir]);
+
+    //retorna o indice do pivo no vetor
     return pivo_index;
 }
 
 int quickselect(int *vetor, int esq, int dir, int k){
+    //caso o vetor tenha apenas um elemento, retorna ele
     if(esq == dir){
         return vetor[esq];
     }
 
+    //seleciona aleatoriamente uma posicao pro pivo
     int pivo_index = esq + rand() % (dir - esq + 1);
+
     pivo_index = particiona(vetor, esq, dir, pivo_index);
 
+    //caso k seja igual a pivo_index, o pivo esta na posicao ordenada
     if(k == pivo_index){
         return vetor[k];
     }
 
+    /*caso k seja menor que o indice do pivo, usa recursao na parte do vetor a esquerda de
+    pivo_index*/
     else if(k < pivo_index){
         return quickselect(vetor, esq, pivo_index-1, k);
     }
 
+    /*caso k seja maior que o indice do pivo, usa recursao na parte do vetor a direita de
+    pivo_index*/
     else{
         return quickselect(vetor, pivo_index+1, dir, k);
     }
+}
+
+//funcao para receber o vetor com numero indeterminado de elementos ate EOF
+int* get_vetor(int *n){
+    int tam = 0;
+    int *vetor = NULL;
+    int valor;
+
+    while(scanf("%d", &valor) != EOF){
+        if(tam == 0){
+            vetor = (int*) malloc(sizeof(int));
+        }
+        else{
+            vetor = (int*) realloc(vetor, (tam+1)*sizeof(int));
+        }
+        vetor[tam] = valor;
+        tam++;
+    }
+
+    *n = tam;
+    return vetor;
 }
 
 int main(int argc, char* argv[]){
@@ -55,16 +90,10 @@ int main(int argc, char* argv[]){
     }
     int k = atoi(argv[2]);
 
-    int tam = 1;
-    int *vetor = (int*) malloc(tam*sizeof(int));
+    int n;
+    int *vetor = get_vetor(&n);
 
-    while(scanf("%d", &vetor[tam-1]) != EOF){
-        tam++;
-        vetor = (int*) realloc(vetor, tam*sizeof(int));
-    }
-    vetor = (int*) realloc(vetor, (tam-1)*sizeof(int));
-
-    printf("%d\n", quickselect(vetor, 0, tam-1, k-1));
+    printf("%d\n", quickselect(vetor, 0, n-1, k-1));
 
     free(vetor);
     return 0;
